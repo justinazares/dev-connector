fa106a1747d92b704803ed52d092061e7719dcfe
justinazares <jzars.webdev@icloud.com>
2018-06-14 12:38:52 -0700

    add Register component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
 
-className Register extends Component {
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    console.log(newUser);
+  }
+
   render() {
     return (
       <div className="register">
@@ -8,15 +39,18 @@ className Register extends Component {
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
-              <p className="lead text-center">Create your DevConnector account</p>
-              <form action="create-profile.html">
+              <p className="lead text-center">
+                Create your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Name"
                     name="name"
-                    required
+                    value={this.state.name}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -25,6 +59,8 @@ className Register extends Component {
                     className="form-control form-control-lg"
                     placeholder="Email Address"
                     name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
                   />
                   <small className="form-text text-muted">
                     This site uses Gravatar so if you want a profile image, use
@@ -37,6 +73,8 @@ className Register extends Component {
                     className="form-control form-control-lg"
                     placeholder="Password"
                     name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -45,6 +83,8 @@ className Register extends Component {
                     className="form-control form-control-lg"
                     placeholder="Confirm Password"
                     name="password2"
+                    value={this.state.password2}
+                    onChange={this.onChange}
                   />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
