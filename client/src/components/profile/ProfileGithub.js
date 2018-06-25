9e71a4fe9040949f6dcdc42a4945829290d0b8aa
justinazares <jzars.webdev@icloud.com>
2018-06-25 14:53:00 -0700

    add Profile Header

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export default class ProfileGithub extends Component {
+class ProfileGithub extends Component {
   render() {
     return (
       <div>
         <h1>TODO: PROFILE GITHUB</h1>
       </div>
-    )
+    );
   }
 }
 
